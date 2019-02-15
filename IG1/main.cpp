173084996fa3e320411eee9b249d39f905898be7
Kyra <kyradiscord@gmail.com>
2019-02-15 16:15:25 +0100

    misc: Folder re-organization

--- IG1/main.cpp
+++ IG1/main.cpp
@@ -4,7 +4,7 @@
 //#include <GL/glut.h>  // OpenGL Utility Toolkit
 
 #include <GL/freeglut.h>  // Free OpenGL Utility Toolkit for creating windows, initializing OpenGL contexts, and handling input events
-#include <glm.hpp>  // OpenGL Mathematics. A C++ mathematics library for graphics programming 
+#include <glm.hpp>  // OpenGL Mathematics. A C++ mathematics library for graphics programming
 
 #include "Viewport.h"
 #include "Camera.h"
@@ -17,13 +17,13 @@ using namespace std;
 //---------- Global variables -------------------------------------------------------------
 
 // Viewport position and size
-Viewport viewPort(800, 600);   
+Viewport viewPort(800, 600);
 
 // Camera position, view volume and projection
-Camera camera(&viewPort);    
+Camera camera(&viewPort);
 
 // Graphics objects of the scene
-Scene scene;   
+Scene scene;
 
 //----------- Callbacks ----------------------------------------------------
 
@@ -44,54 +44,54 @@ int main(int argc, char *argv[])
   glutInitContextVersion(3, 3);
   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);  // GLUT_CORE_PROFILE
   glutInitContextFlags(GLUT_DEBUG);   // GLUT_FORWARD_COMPATIBLE
- 
-  glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS); 
-  
+
+  glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
   glutInitWindowSize(800, 600);   // window size
   //glutInitWindowPosition (140, 140);
 
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH /*| GLUT_STENCIL*/); // RGBA colors, double buffer, depth buffer and stencil buffer   
-  
-  int win = glutCreateWindow("IG1App");  // window's identifier
-  
+  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH /*| GLUT_STENCIL*/); // RGBA colors, double buffer, depth buffer and stencil buffer
+
+  int win = glutCreateWindow("IG1");  // window's identifier
+
   // Callback registration
   glutReshapeFunc(resize);
   glutKeyboardFunc(key);
   glutSpecialFunc(specialKey);
   glutDisplayFunc(display);
- 
+
   cout << glGetString(GL_VERSION) << '\n';
   cout << glGetString(GL_VENDOR) << '\n';
 
   // after creating the context
   camera.set2D();
-  scene.init();    
-  
-  glutMainLoop(); 
-    
-  //cin.ignore(INT_MAX, '\n');  cin.get();  
-  glutDestroyWindow(win);  // Destroy the context 
- 
+  scene.init();
+
+  glutMainLoop();
+
+  //cin.ignore(INT_MAX, '\n');  cin.get();
+  glutDestroyWindow(win);  // Destroy the context
+
   return 0;
 }
 //-------------------------------------------------------------------------
 
 void display()   // double buffering
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);   
-  
-  scene.render(camera);   
-    
-  glutSwapBuffers();  
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  scene.render(camera);
+
+  glutSwapBuffers();
 }
 //-------------------------------------------------------------------------
 
 void resize(int newWidth, int newHeight)
 {
-  // Resize Viewport 
-  viewPort.uploadSize(newWidth, newHeight);  
-  
-  // Resize Scene Visible Area 
+  // Resize Viewport
+  viewPort.uploadSize(newWidth, newHeight);
+
+  // Resize Scene Visible Area
   camera.uploadSize(viewPort.getW(), viewPort.getH());    // scale unchanged
 }
 //-------------------------------------------------------------------------
@@ -101,17 +101,17 @@ void key(unsigned char key, int x, int y)
   bool need_redisplay = true;
 
   switch (key) {
-  case 27:  // Escape key 
-    glutLeaveMainLoop();  // Freeglut's sentence for stopping glut's main loop 
+  case 27:  // Escape key
+    glutLeaveMainLoop();  // Freeglut's sentence for stopping glut's main loop
     break;
-  case '+': 
+  case '+':
     camera.uploadScale(+0.01);   // zoom in  (increases the scale)
     break;
   case '-':
     camera.uploadScale(-0.01);   // zoom out (decreases the scale)
     break;
   case 'l':
-	camera.set3D(); 
+	camera.set3D();
 	break;
   case 'o':
 	camera.set2D();
@@ -135,7 +135,7 @@ void specialKey(int key, int x, int y)
     camera.pitch(1);   // rotate 1 on the X axis
     break;
   case GLUT_KEY_LEFT:
-    camera.yaw(1);     // rotate 1 on the Y axis 
+    camera.yaw(1);     // rotate 1 on the Y axis
     break;
   case GLUT_KEY_UP:
     camera.roll(1);    // rotate 1 on the Z axis
@@ -152,4 +152,3 @@ void specialKey(int key, int x, int y)
     glutPostRedisplay();
 }
 //-------------------------------------------------------------------------
-
